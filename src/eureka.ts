bddd4f45e64da8ea350c0d29af0090fa2a14e740
Melina Figueroa <104174303+Melina-figueroa@users.noreply.github.com>
2022-05-07 12:09:23 -0300

    eureka con while modificacion de ejercicio eureka para ser usado con while

--- src/eureka.ts
+++ src/eureka.ts
@@ -1,11 +1,12 @@
 let clave: string;
 let intentos: number = 1;
-while (intentos <= 3 && clave === "eureka") {
-  clave = prompt("ingresar clave");
-  if (clave === "eureka") {
-    console.log("Bienvenido");
-  } else {
-    console.log("La clave ingresada es incorrecta");
-  }
+
+while (intentos <= 3 && clave != "eureka") {
+  clave = prompt("Ingrese clave");
   intentos++;
+}
+if (clave === "eureka") {
+  console.log("Bienvenido, clave correcta.");
+} else {
+  console.log("Intentos acabados.");
 }

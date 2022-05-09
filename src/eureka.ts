14d25249a66f5f104d9ceedc30f31db3a972d3fd
Melina Figueroa <104174303+Melina-figueroa@users.noreply.github.com>
2022-05-08 22:18:14 -0300

    parOimpar ejercicio de par o impar

--- src/eureka.ts
+++ src/eureka.ts
@@ -1,10 +1,10 @@
 let clave: string;
 let intentos: number = 1;
 
-while (intentos <= 3 && clave != "eureka") {
-  clave = prompt("Ingrese clave");
-  intentos++;
-}
+//while (intentos <= 3 && clave != "eureka") {
+clave = prompt("Ingrese clave");
+intentos++;
+//}
 if (clave === "eureka") {
   console.log("Bienvenido, clave correcta.");
 } else {
--- src/parOimpar.ts
+++ src/parOimpar.ts
@@ -1,5 +1,8 @@
 let numero_ingreso: number = Number(prompt("Ingrese dato"));
-if (numero_ingreso % 2 === 0) {
+
+if (numero_ingreso <= 0) {
+  console.log("¡Ingrese otro número!");
+} else if (numero_ingreso % 2 === 0) {
   console.log("Es par");
 } else {
   console.log("Es impar");
